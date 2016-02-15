a8aca887c6a70d9a5a567b655c8903367142fc1d
Scott Hardy <scott.the.hardy@gmail.com>
2016-02-14 23:25:44 -0800

    Improved helper.js formatting and comments

--- helper.js
+++ helper.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
 var opts = require('minimist')(process.argv.slice(2), {
-  alias: { u: 'username', p: 'password' }
+    alias: { u: 'username', p: 'password' }
 });
 
-// only respond to get actions
+// a `get` action is expected, otherwise exit non-zero
 if (!opts._.length || opts._[0].toLowerCase() !== 'get') process.exit(1);
 
-// load environment variable if specified, default to empty string
+// load value from environment variable if specified, otherwise use blank value
 var username = opts.username ? process.env[opts.username] : '';
 var password = opts.password ? process.env[opts.password] : '';
 
-// exit if specified environment variable doesn't exist
+// exit non-zero if a specified environment variable is not set
 if (username === undefined || password === undefined) process.exit(1);
 
 process.stdout.write(
-  'username=' + username + '\n' +
-  'password=' + password + '\n'
+    'username=' + username + '\n' +
+    'password=' + password + '\n'
 );
