ce07d410a18ba365c0c1d276e019a67d4f7feade
Scott Hardy <scott.the.hardy@gmail.com>
2016-02-14 23:03:19 -0800

    Removed the function declaration from `helper.js`.  Also cleaned up the contents of that file.

--- helper.js
+++ helper.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
 
 var opts = require('minimist')(process.argv.slice(2), {
-  alias: {u: 'username', p: 'password'}
+  alias: { u: 'username', p: 'password' }
 });
 
-envCredential(opts);
+// only respond to get actions
+if (!opts._.length || opts._[0].toLowerCase() !== 'get') process.exit(1);
 
-function envCredential(opts) {
-  // only respond to get actions
-  if (!opts._.length || opts._[0].toLowerCase() !== 'get') process.exit(1);
+// load environment variable if specified, default to empty string
+var username = opts.username ? process.env[opts.username] : '';
+var password = opts.password ? process.env[opts.password] : '';
 
-  var username = opts.username ? process.env[opts.username] : '';
-  var password = opts.password ? process.env[opts.password] : '';
+// exit if specified environment variable doesn't exist
+if (username === undefined || password === undefined) process.exit(1);
 
-  if (username === undefined) process.exit(1);
-  if (password === undefined) process.exit(1);
-
-  process.stdout.write('username=' + username + '\n');
-  process.stdout.write('password=' + password + '\n');
-}
+process.stdout.write(
+  'username=' + username + '\n' +
+  'password=' + password + '\n'
+);
